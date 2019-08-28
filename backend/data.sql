513cf50619c6c8329f44629050da3fd6af1c735b
Stephanie Simms <stephanie.simms@gmail.com>
2019-08-28 10:39:16 -0700

    updated data model to associate calendly user ids with elevate user ids

--- backend/data.sql
+++ backend/data.sql
@@ -24,7 +24,8 @@ CREATE TABLE users (
 CREATE TABLE appointments (
   id serial PRIMARY KEY,
   user_id INTEGER NOT NULL REFERENCES users (id) ON DELETE CASCADE,
-  event_uuid TEXT NOT NULL,
+  event_id TEXT NOT NULL,
+  calendly_user_id TEXT NOT NULL,
   created_at TIMESTAMP,
   updated_at TIMESTAMP,
   event_type TEXT,
@@ -40,6 +41,14 @@ CREATE TABLE appointments (
   canceled_at TIMESTAMP
 );
 
+-- Table for associating elevate user id with calendly user ids
+-- Use this to identify which professional an elevate user has scheduled an appointment with
+-- by collecting the calendly user id from the appointment response object
+CREATE TABLE users_calendly_users (
+ user_id INTEGER NOT NULL REFERENCES users (id),
+ calendly_user_id TEXT NOT NULL
+);
+
 INSERT INTO users (email, password, is_admin, first_name, last_name, current_company, hire_date, needs, goals) VALUES
   ('testuser@gmail.com', 'password123', false, 'Test', 'User', 'Google', '2018-06-23', 'Talk to financial advisor about salary/equity negotiations.', 'Increase in equity.'),
   ('admin@gmail.com', 'admin123', true, 'Admin', 'User', '', '2019-06-23', '', ''),
@@ -47,10 +56,13 @@ INSERT INTO users (email, password, is_admin, first_name, last_name, current_com
   ('elie@gmail.com', 'elie123', false, 'Elie', 'Schoppik', 'Rithm', '2017-06-01', 'Talk to financial advisor to calculate how many instructors he can hire.', 'Recruit more instructors.'),
   ('joel@gmail.com', 'joel123', false, 'Joel', 'Burton', 'Rithm', '2017-08-23', 'General investment advice', 'Help bootcamp grads negotiate.');
 
-INSERT INTO appointments (user_id, event_uuid, created_at, updated_at, event_type, reason, admin_notes, event_date, start_time, end_time, location, canceled, canceler_name, cancel_reason, canceled_at) VALUES
-  (1, 'BCHFF2F62BWNJVPP', '2019-08-29T09:15:00-07:00', null, 'One-on-One', 'legal advice', null, '2019-09-02', '15:00', '15:30', 'Zoom', false, null, null, null),
-  (2, 'GCIEBYAHGKWNENHS', '2019-08-27T14:00:29-07:00', null, 'One-on-One', 'consult with a lawyer', null, '2019-09-19', '10:00', '10:15', 'Zoom', false, null, null, null),
-  (3, 'AAFOAUQKKVOBSZVD', '2019-08-27T16:30:00-07:00', null, 'One-on-One', 'negotiate my salary', null, '2019-09-12', '12:00', '12:30', 'Zoom', false, null, null, null),
-  (4, 'EGMBBYHHDUZTVUKA', '2019-08-27T12:01:01-07:00', '2019-08-27T14:00:29-07:00', 'One-on-One', 'legal advice', null, '2019-09-02', '09:00', '10:00', 'Zoom', true, 'Emi Tsukuda', 'too many meetings', '2019-08-29T09:15:00-07:00'),
-  (5, 'FDLGUGK6SOB54B3G', '2019-08-27T14:00:29-07:00', '2019-08-29T09:15:00-07:00', 'One-on-One', 'consult with a lawyer', null, '2019-08-29', '11:30', '12:00', 'Zoom', true, 'Stephanie Simms', 'meetings hate em', '2019-08-28T09:15:00-07:00');
+INSERT INTO appointments (user_id, event_id, calendly_user_id, created_at, updated_at, event_type, reason, admin_notes, event_date, start_time, end_time, location, canceled, canceler_name, cancel_reason, canceled_at) VALUES
+  (3, 'BCHFF2F62BWNJVPP', 'ABCFF2F62BWNJVPP', '2019-08-29T09:15:00-07:00', null, 'One-on-One', 'legal advice', '', '2019-09-02', '15:00', '15:30', 'Zoom', false, '', '', null),
+  (3, 'GCIEBYAHGKWNENHS', 'BCHFF2F62BWNJVPP', '2019-08-27T14:00:29-07:00', null, 'One-on-One', 'consult with a lawyer', '', '2019-09-19', '10:00', '10:15', 'Zoom', false, '', '', null),
+  (3, 'AAFOAUQKKVOBSZVD', 'BCHFF2F62BWNJVPP', '2019-08-27T16:30:00-07:00', null, 'One-on-One', 'negotiate my salary', '', '2019-09-12', '12:00', '12:30', 'Zoom', false, '', '', null),
+  (4, 'EGMBBYHHDUZTVUKA', 'BCHFF2F62BWNJVPP', '2019-08-27T12:01:01-07:00', '2019-08-27T14:00:29-07:00', 'One-on-One', 'legal advice', '', '2019-09-02', '09:00', '10:00', 'Zoom', true, 'Emi Tsukuda', 'too many meetings', '2019-08-29T09:15:00-07:00'),
+  (5, 'FDLGUGK6SOB54B3G', 'BCHFF2F62BWNJVPP', '2019-08-27T14:00:29-07:00', '2019-08-29T09:15:00-07:00', 'One-on-One', 'consult with a lawyer', '', '2019-08-29', '11:30', '12:00', 'Zoom', true, 'Stephanie Simms', 'meetings hate em', '2019-08-28T09:15:00-07:00');
 
+INSERT INTO users_calendly_users (user_id, calendly_user_id) VALUES
+  (1, 'ABCFF2F62BWNJVPP'),
+  (2, 'BCHFF2F62BWNJVPP');
