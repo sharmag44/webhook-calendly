d8ca2702fcd5d63b26f3332102e9aa1cded639bc
Stephanie Simms <stephanie.simms@gmail.com>
2019-08-29 13:45:45 -0700

    cleaned up app.js comments and doc string

--- backend/app.js
+++ backend/app.js
@@ -12,22 +12,19 @@ const appointmentSchema = require("./schemas/appointmentSchema.json");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 // Endpoint for webhook to receive calendly events
-// TODO: after testing and before integrating into elevate update route to '/webhook' 
-app.post('/', async function (req, res, next) {
+// To test webhook/calendly functionality locally with ngrok you must configure this endpoint as '/'
+app.post('/webhook', async function (req, res, next) {
   try {
     // parse request payload then validate
     const eventPayload = req.body.payload;
     const parsedObj = parseResponse(eventPayload);
     const validate = jsonschema.validate(parsedObj, appointmentSchema);
 
-    console.log("validate.valid", validate.valid)
     if (!validate.valid) {
       let listOfErrors = validate.errors.map(error => error.stack);
       throw new ExpressError(listOfErrors, 400);
     }
-    console.log("req.body.event", req.body.event)
     // Handle the event
     switch (req.body.event) {
       case 'invitee.created':
@@ -40,10 +37,8 @@ app.post('/', async function (req, res, next) {
         // Unexpected event type
         return res.status(400).end();
     }
-
     // Return a response to acknowledge receipt of the event
     res.json({ received: true });
-
   }
   catch (err) {
     return next(err);
