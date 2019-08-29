04843922fd29a36a9b58519e2d751c00b22d7bfd
Stephanie Simms <stephanie.simms@gmail.com>
2019-08-29 12:10:31 -0700

    webhook endpoint and validation are working, need to adjust error handling

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const { parseResponse } = require("./helperWebhook");
 const { create, cancel} = require("./models/appointment");
 
 const jsonschema = require("jsonschema");
-const appointmentSchema = require("../schemas/appointmentSchema.json");
+const appointmentSchema = require("./schemas/appointmentSchema.json");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -16,20 +16,20 @@ app.use(express.urlencoded({ extended: true }));
 // Endpoint for webhook to receive calendly events
 // TODO: after testing and before integrating into elevate update route to '/webhook' 
 app.post('/', async function (req, res, next) {
-  // parse req.body then validate
-  const event = req.body.payload;
-  const parsedObj = parseResponse(event);
-
   try {
+    // parse request payload then validate
+    const eventPayload = req.body.payload;
+    const parsedObj = parseResponse(eventPayload);
     const validate = jsonschema.validate(parsedObj, appointmentSchema);
 
+    console.log("validate.valid", validate.valid)
     if (!validate.valid) {
       let listOfErrors = validate.errors.map(error => error.stack);
       throw new ExpressError(listOfErrors, 400);
     }
-
+    console.log("req.body.event", req.body.event)
     // Handle the event
-    switch (event.event) {
+    switch (req.body.event) {
       case 'invitee.created':
         await create(parsedObj);
         break;
